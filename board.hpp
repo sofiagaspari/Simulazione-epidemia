ebff487e9437d858df6cc05ef19bc5b241e73236
sofiagaspari <102584445+sofiagaspari@users.noreply.github.com>
2022-08-27 16:07:59 +0200

    Create board.hpp

---
+++ board.hpp
@@ -0,0 +1,31 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+#include <vector>
+
+enum class Diag : char { s, i, r };
+typedef std::vector<Diag> line;
+
+class Board {
+private:
+  int dimension_;
+  std::vector<line> grid;
+  double beta_;
+  double gamma_;
+  int count_time;
+  int inf;
+  int rim;
+  int days;
+  int num_i;
+  int num_s;
+  int num_r;
+  double inf_prob;
+
+public:
+  Board(int s, int i, int r, double b, double y, int days_);
+  void evolve();
+  int return_beta(double prob);
+  int return_gamma();
+  void draw();
+};
+
+#endif
