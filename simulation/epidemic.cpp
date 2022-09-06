86b8ff94b3e78f4b7850a44e5f690f3af008e3fa
ElenaBonali <102584308+ElenaBonali@users.noreply.github.com>
2022-09-06 16:11:03 +0200

    Update epidemic.cpp

--- simulation/epidemic.cpp
+++ simulation/epidemic.cpp
@@ -1,37 +1,39 @@
 #include "epidemic.hpp"
-#include <vector>
+#include <cassert>
 
-Epidemic::Epidemic(int s , int i , int r,double b, double y , int d )
-:
-    memory {State{s,i,r}},
-    beta {b},
-    gamma {y},
-    tot {s + i + r},
-    days {d}
-    {}
-    ;
+Epidemic::Epidemic(int s, int i, int r, double b, double y,
+                   int d)      
+    : memory{State{s, i, r}}, 
+      beta{b},
+      gamma{y},
+      tot{s + i + r},
+      days{d} {};
 
-State Epidemic::Evolve (State const& start) {
-    State end;
-    end.sus_=start.sus_-beta*start.sus_*start.inf_/tot;
-    end.inf_=start.inf_+beta*start.sus_*start.inf_/tot-gamma*start.inf_;
-    end.rim_=start.rim_+gamma*start.inf_;
-return end;
-
+State Epidemic::Evolve(State const &start) {  
+  State end;
+  end.sus_ = start.sus_ -
+             beta * start.sus_ * start.inf_ / tot; 
+  end.inf_ =
+      start.inf_ + beta * start.sus_ * start.inf_ / tot - gamma * start.inf_;
+  end.rim_ = start.rim_ + gamma * start.inf_;
+  assert(end.sus_ >= 0);
+  assert(end.inf_ >= 0);
+  assert(end.rim_ >= 0);
+  return end;
 };
-void Epidemic::Update (int d){
-for (int i = 0; i < d; ++i) {
+void Epidemic::Update(int d) {  
+  for (int i = 0; i < d; ++i) {
     memory.push_back(Evolve(memory[i]));
   }
 };
 
-  int Epidemic::S_get (int i) const {
-    return memory[i].sus_;};
-
-    int Epidemic::I_get (int i) const  {
-        return memory[i].inf_;
-  };
-int Epidemic::R_get (int i) const  { 
-    return memory[i].rim_;
+int Epidemic::S_get(int i) const { 
+  return memory[i].sus_;
 };
 
+int Epidemic::I_get(int i) const { 
+  return memory[i].inf_;
+};
+int Epidemic::R_get(int i) const {  
+  return memory[i].rim_;
+};
