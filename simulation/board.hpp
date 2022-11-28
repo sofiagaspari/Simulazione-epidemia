0ac8f13c712f2e2c71be0e62a7ea7920d7c5db08
sofiagaspari <102584445+sofiagaspari@users.noreply.github.com>
2022-11-28 20:28:47 +0100

    Update board.hpp

--- simulation/board.hpp
+++ simulation/board.hpp
@@ -5,23 +5,21 @@
 
 
 enum class Diag : char { s, i, r };
-typedef std::vector<Diag> line;
-inline std::random_device dev;
-inline std::mt19937 generator(dev());
-inline std::uniform_real_distribution<double> distr(0.0, 1.0);
+using line = std::vector<Diag>;
+
 class Board {
  private:
   int dimension_;
   std::vector<line> grid;
   double beta_;
   double gamma_;
-  int count_time;
+  int count_time=0;
   int inf;
   int rim;
   int days;
   int num_i;
   int num_s;
-  int num_r;
+  int num_r=0;
 
  public:
   Board(int s, int i, int r, double b, double y, int days_);
